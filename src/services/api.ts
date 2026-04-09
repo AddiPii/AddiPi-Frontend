c7e384380b91fb50894d910994f6665b0e72d742
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-04-09 15:21:49 +0200

    test branch with test printer url

--- src/services/api.ts
+++ src/services/api.ts
@@ -13,7 +13,7 @@ import type {
 
 const AUTH_URL = `https://addipi-auth-service.vercel.app`;
 const USER_URL = `https://addipi-user-service.vercel.app`;
-const PRINTER_URL = `https://addipi-printer-service.vercel.app`;
+const PRINTER_URL = `https://addipi-printer-service-git-dev-28e89f-oliwers-projects-d5e8db11.vercel.app`;
 const FILES_URL = `https://addipi-files-service.vercel.app`;
 
 class ApiClient {
